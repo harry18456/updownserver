711c344ef97e87d26fd912f84dfd336af184742b
harry18456 <harry18456@gmail.com>
2026-01-20 02:24:18 +0800

    feat: remove standalone GET /upload page

--- updownserver/__init__.py
+++ updownserver/__init__.py
@@ -18,61 +18,7 @@ COLOR_SCHEME = {
     'dark': 'dark',
 }
 
-def get_upload_page(theme: str) -> bytes:
-    return bytes('''<!DOCTYPE html>
-<html>
-<head>
-<title>File Upload</title>
-<meta name="viewport" content="width=device-width, user-scalable=no" />
-<meta name="color-scheme" content="''' + COLOR_SCHEME.get(theme) + '''">
-</head>
-<body>
-<h1>File Upload</h1>
-<form action="upload" method="POST" enctype="multipart/form-data">
-<input name="files" type="file" multiple />
-<br />
-<br />
-<input type="submit" />
-</form>
-<p id="task"></p>
-<p id="status"></p>
-</body>
-<script>
-document.getElementsByTagName('form')[0].addEventListener('submit', async e => {
-  e.preventDefault()
-  
-  const uploadFormData = new FormData(e.target)
-  const filenames = uploadFormData.getAll('files').map(v => v.name).join(', ')
-  const uploadRequest = new XMLHttpRequest()
-  uploadRequest.open(e.target.method, e.target.action)
-  uploadRequest.timeout = 3600000
-  
-  uploadRequest.onreadystatechange = () => {
-    if (uploadRequest.readyState === XMLHttpRequest.DONE) {
-      let message = `${uploadRequest.status}: ${uploadRequest.statusText}`
-      if (uploadRequest.status === 0) message = 'Connection failed'
-      if (uploadRequest.status === 204) {
-        message = `Success: ${uploadRequest.statusText}`
-      }
-      document.getElementById('status').textContent = message
-    }
-  }
-  
-  uploadRequest.upload.onprogress = e => {
-    document.getElementById('status').textContent = (e.loaded === e.total ?
-      'Saving…' :
-      `${Math.floor(100*e.loaded/e.total)}% ` +
-      `[${Math.floor(e.loaded/1024)} / ${Math.floor(e.total/1024)}KiB]`
-    )
-  }
-  
-  uploadRequest.send(uploadFormData)
-  
-  document.getElementById('task').textContent = `Uploading ${filenames}:`
-  document.getElementById('status').textContent = '0%'
-})
-</script>
-</html>''', 'utf-8')
+
 
 def get_directory_head_injection(theme: str) -> bytes:
     return bytes('''<!-- Injected by updownserver -->
@@ -330,12 +276,7 @@ DIRECTORY_BODY_INJECTION = b'''<!-- Injected by updownserver -->
 <!-- End injection by updownserver -->
 '''
 
-def send_upload_page(handler: http.server.BaseHTTPRequestHandler):
-    handler.send_response(http.HTTPStatus.OK)
-    handler.send_header('Content-Type', 'text/html; charset=utf-8')
-    handler.send_header('Content-Length', len(get_upload_page(args.theme)))
-    handler.end_headers()
-    handler.wfile.write(get_upload_page(args.theme))
+
 
 class PersistentFieldStorage(cgi.FieldStorage):
     # Override cgi.FieldStorage.make_file() method. Valid for Python 3.1 ~ 3.10.
@@ -630,10 +571,7 @@ class CGIHTTPRequestHandler(ListDirectoryInterception,
     def do_GET(self):
         if not check_http_authentication(self): return
         
-        if self.path == '/upload':
-            send_upload_page(self)
-        else:
-            super().do_GET()
+        super().do_GET()
     
     def do_POST(self):
         if not check_http_authentication(self): return
